5e680c3cf16b9cff399feabc3c8b92d8cbd0da58
skjo0c <skj.jo0c@gmail.com>
2016-09-05 12:12:58 +0545

    models_and_controllers

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+#carrierwave
+gem 'carrierwave'
+#devise
+gem 'devise'
 #Bootstrap
 gem 'bootstrap-sass', '~> 3.3.6'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name, :photo) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :name, :photo) }
+  end
+end
 end
